70e8824075f737b3e2050e9212581ebc7628be24
kentaroMiura <oikimutida@yahoo.co.jp>
2017-03-04 21:15:43 +0900

    [add] Add project file

---
+++ objective-c_sample/objective-c_sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objective-c_sample
+//
+//  Created by Miura Kentaro on 2017/03/04.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objective-c_sample/objective-c_sample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  objective-c_sample
+//
+//  Created by Miura Kentaro on 2017/03/04.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
