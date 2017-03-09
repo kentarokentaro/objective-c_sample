d16f91de8e448ce5e3722a044e85096a57153c9e
kentarokentaro <oikimutida@yahoo.co.jp>
2017-03-09 11:35:47 +0900

    [UITableView_Sample][modify] Add basic UITableView

--- objective-c_sample/objective-c_sample/ViewController.m
+++ objective-c_sample/objective-c_sample/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -25,5 +26,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc]
+                             initWithStyle:UITableViewCellStyleDefault
+                             reuseIdentifier:@"Cell"];
+return cell;
+}
+
+#pragma mark - Table view delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+}
 
 @end
