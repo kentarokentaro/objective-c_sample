e71562553df164f197cf69f11b8b65a48a405705
kentarokentaro <oikimutida@yahoo.co.jp>
2017-03-15 18:12:02 +0900

    [UITableView_Sample][add] Add manager class for the management of each class name displayed in TableVIewCell

---
+++ objective-c_sample/objective-c_sample/ViewManager.h
@@ -0,0 +1,12 @@
+//
+//  ViewManager.h
+//  objective-c_sample
+//
+//  Created by Careritz on 2017/03/09.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ViewManager : NSObject
+@end
---
+++ objective-c_sample/objective-c_sample/ViewManager.m
@@ -0,0 +1,13 @@
+//
+//  ViewManager.m
+//  objective-c_sample
+//
+//  Created by Careritz on 2017/03/09.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+#import "ViewManager.h"
+
+@implementation ViewManager
+
+@end
