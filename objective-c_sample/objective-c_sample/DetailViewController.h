97a89d0065b54c76b4ee77530af081917d1ff8c6
kentarokentaro <oikimutida@yahoo.co.jp>
2017-04-04 15:34:50 +0900

    [add] Add DetailViewController file

---
+++ objective-c_sample/objective-c_sample/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  objective-c_sample
+//
+//  Created by Careritz on 2017/04/04.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
