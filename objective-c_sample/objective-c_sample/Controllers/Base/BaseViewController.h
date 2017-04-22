cb2ec8cb33cfce0e84a2ba501f26a1568cc849d1
kentaroMiura <oikimutida@yahoo.co.jp>
2017-04-22 17:30:56 +0900

    [add] Add BaseViewController class

---
+++ objective-c_sample/objective-c_sample/Controllers/Base/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  objective-c_sample
+//
+//  Created by Miura Kentaro on 2017/04/22.
+//  Copyright © 2017年 Miura Kentaro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
